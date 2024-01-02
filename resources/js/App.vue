b77d1bb667c9c6e4c52fe55a587edbb213545a17
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-01-03 01:49:01 +0600

    service list style done

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="row justify-content-center align-items-center mt-5">
     <div class="col-6 card p-5">
-      <h2 class="bg-info-subtle mb-3 py-4 text-center">Vue Service App</h2>
+      <h2 class="bg-info-subtle mb-5 py-4 text-center">Vue Service App</h2>
+
+      <div>
+        <ul class="list-group">
+          <li class="list-group-item text-center">
+            <a href="" class="text-decoration-none">Weather App</a>
+          </li>
+          <li class="list-group-item text-center">
+            <a href="" class="text-decoration-none">User CRUD</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-
-
 </script>

12710999304e2eb63b1452320164015dc1b2c35f
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-01-04 02:16:44 +0600

    vue router added

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,12 @@ import './bootstrap';
 
 import {createApp} from 'vue';
 
+import router from './router';
+
 import App from './App.vue';
 
 const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
---
+++ resources/js/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        component: () => import(/* webpackChunkName: "Home" */ '../views/home.vue'),
+    },
+    {
+        path: "/weather-report",
+        name: "weatherReport",
+        component: () => import(/* webpackChunkName: "ProductCategories" */ '../services/weather-app/index.vue'),
+    },
+    {
+        path: "/404",
+        name: "PageNotExist",
+        component: () => import(/* webpackChunkName: "ErrorPage" */ '../views/Error.vue'),
+    },
+    {
+        path: "/:catchAll(.*)", // Unrecognized path automatically matches 404
+        redirect: "/404",
+    }
+];
+
+const router = createRouter({
+    routes: routes,
+    scrollBehavior: function (to, _from, savedPosition) {
+        return window.scrollTo(0, 0);
+    },
+    history: createWebHistory('/'),
+})
+
+export default router;
